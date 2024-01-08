9e7d393dbf6e8e39828cd0ae657fcfa689d384b0
surajkareppagol <surajkareppagol.dev@gmail.com>
2024-01-08 23:03:49 +0530

    🔢 Add Problem 13, 1941

---
+++ 1941. Check if All Characters Have Equal Number of Occurrences/main.py
@@ -0,0 +1,18 @@
+def areOccurrencesEqual(s: str) -> bool:
+    occurrences = {}
+
+    for i in s:
+        if i not in occurrences.keys():
+            occurrences[i] = 1
+        else:
+            occurrences[i] += 1
+
+    initial_value = occurrences[s[0]]
+
+    for c in occurrences.values():
+        if c != initial_value:
+            return False
+
+    return True
+
+print(areOccurrencesEqual("aab"))
---
+++ README.md
@@ -0,0 +1,8 @@
+# Leetcode Problems
+
+Yep, solving `Leetcode Problems` and keeping a track.
+
+## Problems Solved Till Now
+
+- 13\. Roman To Integer
+- 1941\. Check if All Characters Have Equal Number of Occurrences
