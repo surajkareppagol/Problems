24e94ab70a3f15b4d391fe1421f1c24a00382e9a
surajkareppagol <surajkareppagol.dev@gmail.com>
2024-05-13 11:02:19 +0530

    🔢 Leetcode update solution 2373

--- Leetcode/2373. Largest Local Values in a Matrix/main.py
+++ Leetcode/2373. Largest Local Values in a Matrix/main.py
@@ -51,6 +51,50 @@ class Solution:
 
 
 # ====================== #
+# Minor Changes
+
+# class Solution:
+#     def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
+#         # 1. Get the result grid size
+#         n_ = len(grid) - 2
+#         row_ = 0
+#         column_ = 0
+
+#         grid_ = [[0 for _ in range(n_)] for _ in range(n_)]
+#         grid_row = 0
+#         grid_column = 0
+
+#         # 2. Set row and column origins
+#         column_origin = 0
+#         row_origin = 0
+
+#         # 3. For each strip get the value
+#         for _ in range(n_):
+#             for _ in range(n_):
+#                 max_ = 0
+
+#                 for i in range(row_, row_ + 3):
+#                     for j in range(column_, column_ + 3):
+#                         if grid[i][j] > max_:
+#                             max_ = grid[i][j]
+
+#                 grid_[grid_row][grid_column] = max_
+
+#                 row_ += 1
+#                 column_ = column_origin
+#                 grid_row += 1
+
+#             # 4. Change to new strip
+#             row_ = row_origin
+#             column_origin += 1
+#             column_ = column_origin
+#             grid_row = 0
+#             grid_column += 1
+
+#         return grid_
+
+
+# ====================== #
 
 result = Solution().largestLocal(
     [[9, 9, 8, 1], [5, 6, 2, 6], [8, 2, 6, 4], [6, 2, 2, 2]]
